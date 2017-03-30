8819b632adc56e02156422d5d6ef781026157410
randyjp <randyjp125@gmail.com>
2017-03-29 23:44:11 -0400

    rendering image

--- app/components/Camper.jsx
+++ app/components/Camper.jsx
@@ -4,13 +4,15 @@ export class Camper extends React.Component {
   render() {
     var {username, img, alltime, recent, lastUpdate} = this.props;
 
-    var renderImage = (img) => {
-      return 'img';
+    var renderImage = (img, username) => {
+      return (
+        <img alt={username} src={img} height='45px' width='45px'/>
+      );
     };
 
     return (
       <tr>
-         <td>{renderImage(img)}</td>
+         <td>{renderImage(img, username)}</td>
          <td>{username}</td>
          <td>{recent}</td>
          <td>{alltime}</td>
