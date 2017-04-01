f0362348f4fc18c4a3940325c2cad04973466bd6
randyjp <randyjp125@gmail.com>
2017-04-01 17:34:34 -0400

    refactorin camperList to a functional component

--- app/components/CamperList.jsx
+++ app/components/CamperList.jsx
@@ -1,51 +1,50 @@
 import React from 'react';
 import Camper from 'Camper';
 
-export class CamperList extends React.Component {
-  render() {
-    const {campers} = this.props;
-    const renderCampersTable = () => {
-      if (campers.length ===0 ){
-        // TODO: make a nice empty placeholder
-        return (
-          <p>nothing here</p>
-        );
-      }
+export const CamperList = props => {
+  const {campers} = props;
 
-      var finalCampers = campers.map((camper) => {
-        return (
-          <Camper key={camper.username} {...camper}/>
-        );
-      });
-
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>Avatar</th>
-              <th>Name</th>
-              <th>Points in past 30 days</th>
-              <th>All time Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            {finalCampers}
-          </tbody>
-        </table>
-      );
-
-    };
-
-    return (
-      <div>
-        {renderCampersTable()}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {renderCampersTable(campers)}
+    </div>
+  );
+};
 
 CamperList.propTypes = {
   campers: React.PropTypes.array.isRequired
+};
+
+const renderCampersTable = (campers) => {
+  if (campers.length ===0 ){
+    // TODO: make a nice empty placeholder
+    return (
+      <p>nothing here</p>
+    );
+  }
+
+  const finalCampers = campers.map((camper) => {
+    return (
+      <Camper key={camper.username} {...camper}/>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Avatar</th>
+          <th>Name</th>
+          <th>Points in past 30 days</th>
+          <th>All time Points</th>
+        </tr>
+      </thead>
+      <tbody>
+        {finalCampers}
+      </tbody>
+    </table>
+  );
+
 };
 
 export default CamperList;
